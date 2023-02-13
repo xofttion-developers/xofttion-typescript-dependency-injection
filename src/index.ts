55290c1f33f76ee1ec1a3e84814fe0983a224574
ing.dacastillop <ing.dacastillop@gmail.com>
2023-02-13 16:42:37 -0500

    :sparkles: Enabled class Context in Project

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { InjectionFactory } from './factory';
 export { InjectVar, InjectRef, Factory, Singleton } from './decorators';
 export { createInject, createInjectable } from './factory';
 export { InjectLocator } from './stores';
-export { InjectLocatorRef, OnContext } from './types';
+export { ContextType, InjectLocatorRef, OnContext } from './types';
 
 export default InjectionFactory;
--- src/types/context.type.ts
+++ src/types/context.type.ts
@@ -5,3 +5,17 @@ export interface OnContext {
 
   getValue<T = unknown>(key: string): T | undefined;
 }
+
+export class Context implements OnContext {
+  protected context?: ContextType;
+
+  public setContext(context: ContextType): void {
+    this.context = context;
+  }
+
+  getValue<T = unknown>(key: string): T | undefined {
+    return this.context && this.context.has(key)
+      ? (this.context.get(key) as T)
+      : undefined;
+  }
+}
